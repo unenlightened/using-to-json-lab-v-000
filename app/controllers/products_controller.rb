5d8b8b5caeaf3cd7771868b96c920aa945c0c993
Chely Ho <adept_inept@yahoo.com>
2018-07-25 19:46:26 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,10 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render :json @post.to_json }
+    end 
   end
 
   def data
